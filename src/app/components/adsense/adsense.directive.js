37ec8b21f978c6ea400d27e4b48efd1254c081ef
cooper <scttcper@gmail.com>
2015-06-02 11:18:23 -0700

    snappy

--- src/app/components/adsense/adsense.directive.js
+++ src/app/components/adsense/adsense.directive.js
@@ -14,12 +14,12 @@ class adsense {
         return directive;
 
         function linkFunc(scope, el, attr, vm) {
+            let html = `<ins class='adsbygoogle' style='display:block;min-height:60px' data-ad-client='ca-pub-7640562161899788' data-ad-slot='7259870550' data-ad-format='auto' data-ad-region='page-${vm.random}'></ins><br>`;
+            el.append(html);
             vm.$timeout(function() {
-                let html = `<ins class='adsbygoogle' style='display:block;min-height:60px' data-ad-client='ca-pub-7640562161899788' data-ad-slot='7259870550' data-ad-format='auto' data-ad-region='page-${vm.random}'></ins><br>`;
-                el.append(html);
                 let adsbygoogle = window.adsbygoogle || [];
                 adsbygoogle.push({});
-            }, 250, false);
+            }, 10, false);
         }
     }
 }
