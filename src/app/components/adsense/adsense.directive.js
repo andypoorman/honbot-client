ed4d35cbe453b1e253435420f765aa2359171b3d
cooper <scttcper@gmail.com>
2015-06-02 11:15:49 -0700

    formatting

--- src/app/components/adsense/adsense.directive.js
+++ src/app/components/adsense/adsense.directive.js
@@ -15,7 +15,7 @@ class adsense {
 
         function linkFunc(scope, el, attr, vm) {
             vm.$timeout(function() {
-                let html = `<ins class='adsbygoogle' style='display:block' data-ad-client='ca-pub-7640562161899788' data-ad-slot='7259870550' data-ad-format='auto' data-ad-region='page-${vm.random}'></ins><br>`;
+                let html = `<ins class='adsbygoogle' style='display:block;min-height:60px' data-ad-client='ca-pub-7640562161899788' data-ad-slot='7259870550' data-ad-format='auto' data-ad-region='page-${vm.random}'></ins><br>`;
                 el.append(html);
                 let adsbygoogle = window.adsbygoogle || [];
                 adsbygoogle.push({});
