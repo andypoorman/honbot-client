a0c102546d993710daf9ee08148b494f0332cb41
cooper <scttcper@gmail.com>
2015-06-02 01:22:18 -0700

    refactor match

--- src/app/match/match.controller.js
+++ src/app/match/match.controller.js
@@ -1,22 +1,23 @@
 class MatchCtrl {
-    constructor($routeParams, ApiService, heroData, BaseUrl, $scope, $alert, itemList) {
+    constructor($routeParams, ApiService, heroData, $scope, $alert, itemList) {
         'ngInject';
+
         let vm = this;
 
-        vm.BaseUrl = BaseUrl.host;
-        vm.heroData = {};
-        vm.team1 = {};
-        vm.team2 = {};
-        vm.ptips = {};
-        vm.items = itemList;
+        this.heroData = {};
+        this.team1 = {};
+        this.team2 = {};
+        this.ptips = {};
+        this.items = itemList;
+        this.matchid = $routeParams.match;
+        this.$scope = $scope;
 
-        var modes = {
+        this.modes = {
             1: 'rnk',
             2: 'cs',
             3: 'acc'
         };
-
-        vm.options = [
+        this.options = [
             {value: 'kills', label: 'Kills'},
             {value: 'deaths', label: 'Deaths'},
             {value: 'assists', label: 'Assists'},
@@ -34,42 +35,19 @@ class MatchCtrl {
             vm.heroData[_.keys(n)[0]] = n[_.keys(n)[0]];
         });
 
-        var regraph = function(){
-            MG.data_graphic({
-                title: $scope.selectedGraph.label,
-                data: vm.match.players,
-                chart_type: 'bar',
-                x_accessor: 'nickname',
-                y_accessor: $scope.selectedGraph.value,
-                full_width: true,
-                height: 350,
-                left: 30,
-                top: 10,
-                right: 0,
-                target: '#graph',
-                bar_orientation: 'vertical',
-            });
-        };
 
-        var teamtotals = function(){
-            // Loops over every property of every player and creates team totals
-            _.forEach(vm.match.players, function(n) {
-                _.forEach(n, function(j, key) {
-                    if (!vm[`team${n.team}`][key]) {
-                        vm[`team${n.team}`][key] = Number(j);
-                    } else {
-                        vm[`team${n.team}`][key] += Number(j);
-                    }
-                });
-            });
-        };
+        this.activate($scope, $alert, ApiService);
 
-        ApiService.match($routeParams.match, function(res){
+    }
+    activate($scope, $alert, ApiService) {
+        let vm = this;
+
+        ApiService.match(this.matchid, function(res) {
             vm.match = res;
             vm.duration = moment.duration(res.length, 'seconds').format();
-            vm.m = modes[res.mode];
+            vm.m = vm.modes[res.mode];
 
-            if(res.players){
+            if (res.players) {
                 var pids = _.pluck(res.players, 'player_id').join(',');
                 ApiService.bulkPlayers(pids).success(res => {
                     _.forEach(res, (n) => {
@@ -77,11 +55,11 @@ class MatchCtrl {
                     });
                 });
             }
-            
+
             vm.match.players = _.sortBy(vm.match.players, 'position');
-            regraph();
-            teamtotals();
-        }, function(){
+            vm.regraph();
+            vm.teamtotals();
+        }, function() {
             $alert({
                 title: 'ERROR:',
                 content: 'Match not available. Try again maybe later.',
@@ -92,12 +70,41 @@ class MatchCtrl {
             });
         });
 
-        $scope.$watch('selectedGraph', function(newval, oldval){
-            if(newval !== oldval && vm.match){
-                regraph();
+        $scope.$watch('selectedGraph', function(newval, oldval) {
+            if (newval !== oldval && vm.match) {
+                vm.regraph();
             }
         });
 
+    }
+    regraph() {
+        let options = {
+            title: this.$scope.selectedGraph.label,
+            data: this.match.players,
+            chart_type: 'bar',
+            x_accessor: 'nickname',
+            y_accessor: this.$scope.selectedGraph.value,
+            full_width: true,
+            height: 350,
+            left: 30,
+            top: 10,
+            right: 0,
+            target: '#graph',
+            bar_orientation: 'vertical',
+        };
+        MG.data_graphic(options);
+    }
+    teamtotals() {
+        let vm = this;
+        _.forEach(vm.match.players, function(n) {
+            _.forEach(n, function(j, key) {
+                if (!vm[`team${n.team}`][key]) {
+                    vm[`team${n.team}`][key] = Number(j);
+                } else {
+                    vm[`team${n.team}`][key] += Number(j);
+                }
+            });
+        });
     }
 }
 
