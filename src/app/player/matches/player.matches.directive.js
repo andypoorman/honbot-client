41f0dba5d8214a480faac1a838d0f44eb6cf178e
cooper <scttcper@gmail.com>
2015-06-04 00:35:46 -0700

    the mix of $scope and ctrl is ugly

--- src/app/player/matches/player.matches.directive.js
+++ src/app/player/matches/player.matches.directive.js
@@ -16,21 +16,29 @@ class PlayerMatchesDirective {
 }
 
 class PlayerMatchesCtrl {
-    constructor($location, $routeParams, ApiService, $alert, heroData) {
+    constructor($location, $routeParams, ApiService, $alert, heroData, $scope) {
         'ngInject';
 
         this.m = $routeParams.mode || 'rnk';
         this.nickname = $routeParams.player;
         this.$location = $location;
+        this.$scope = $scope;
 
-        this.activate(ApiService, $alert);
+        let that = this;
+        this.heroData = [];
+        _.forEach(heroData, function(n) {
+            that.heroData[_.keys(n)[0]] = n[_.keys(n)[0]];
+        });
+
+        this.activate(ApiService, $alert, $scope);
     }
-    activate(ApiService, $alert) {
+    activate(ApiService, $alert, $scope) {
         ApiService.singlePlayer(this.nickname).success(player => {
             this.player = player;
             ApiService.playerCache(player.account_id, this.m).success(res => {
                 this.all = res;
                 this.setup();
+                that.issetup = true;
             });
         }).error((res) => {
             $alert({
@@ -41,6 +49,13 @@ class PlayerMatchesCtrl {
                 type: 'danger',
                 show: true
             });
+        });
+
+        let that = this;
+        $scope.$watch('selectedHero', function() {
+            if (that.issetup) {
+                that.filter();
+            }
         });
     }
     setup() {
@@ -54,11 +69,27 @@ class PlayerMatchesCtrl {
             temp.version = obj.version;
             temp.match_id = obj.id;
             that.pulled.push(temp);
+            that.heroes.push({
+                value: temp.hero_id,
+                label: that.heroData[temp.hero_id].disp_name
+            });
         });
+        that.heroes = _.chain(that.heroes).uniq('value').sortBy('label').value();
         that.filter();
     }
     filter() {
-        this.filtered = this.pulled;
+        let that = this;
+        let temp = this.pulled;
+        temp = _.filter(temp, function(n) {
+            if(!that.$scope.selectedHero){
+                return n;
+            }
+            if (_.includes(that.$scope.selectedHero, n.hero_id)) {
+                return n;
+            }
+        });
+        this.filtered = temp;
+        console.log(temp)
         this.totals();
     }
     totals() {
