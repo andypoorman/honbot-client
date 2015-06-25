fa830ed59a14d8da119ad281c7eb85bdaafa08ad
cooper <scttcper@gmail.com>
2015-06-25 01:24:49 -0700

    trends cleanup

--- src/app/player/trends/player.trends.directive.js
+++ src/app/player/trends/player.trends.directive.js
@@ -31,14 +31,14 @@ class PlayerMatchesCtrl {
 
         this.activate(ApiService, $alert, $scope, heroData);
     }
-    activate(ApiService, $alert, $scope, heroData) {
+    activate(ApiService, $alert, $scope) {
         ApiService.singlePlayer(this.nickname).success(res => {
             this.curmmr = res[this.m + '_amm_team_rating'] || res[this.m + '_pub_skill'];
             this.account_id = res.account_id;
 
             ApiService.playerCache(this.account_id, this.m).success(res => {
                 this.all = res;
-                this.setup(heroData);
+                this.setup();
                 let options = {
                     title: '',
                     data: this.mmr_history,
@@ -69,10 +69,8 @@ class PlayerMatchesCtrl {
             }
         });
     }
-    setup(heroData) {
-        let heroes = {};
-        let versions = new Set();
-        _.forEach(this.all, (obj) => {
+    setup() {
+        for (let obj of this.all) {
             let temp = _.find(obj.players, 'player_id', this.account_id);
             if (temp) {
                 temp.date = moment(obj.date).toDate();
@@ -88,51 +86,8 @@ class PlayerMatchesCtrl {
                 });
                 this.curmmr -= temp.mmr_change;
                 this.count--;
-
-                // collect all used heroes
-                heroes[temp.hero_id] = {
-                    value: temp.hero_id,
-                    label: heroData[temp.hero_id].disp_name
-                };
-                versions.add(obj.version);
             }
-        });
-        this.heroes = _.sortBy(_.values(heroes), 'label');
-        this.versions = Array.from(versions);
-        this.filter();
-    }
-    filter() {
-        this.filtered = _.filter(this.pulled, (n) => {
-            if (this.filterHero(n.hero_id) && this.filterVersion(n.version)) {
-                return n;
-            }
-        });
-        this.totals();
-    }
-    filterVersion(version) {
-        if (this.$scope.selectedVersion && this.$scope.selectedVersion.length > 0 && !_.includes(this.$scope.selectedVersion, version)) {
-            return false;
         }
-        return true;
-    }
-    filterHero(hero_id) {
-        if (this.$scope.selectedHero && this.$scope.selectedHero.length > 0 && !_.includes(this.$scope.selectedHero, hero_id)) {
-            return false;
-        }
-        return true;
-    }
-    totals() {
-        let needed = ['length', 'kills', 'deaths', 'assists', 'cs', 'denies', 'gpm', 'xpm', 'apm', 'wards', 'herodmg', 'bdmg'];
-        this.averages = {};
-        _.forEach(this.filtered, (n) => {
-            _.forEach(needed, (j) => {
-                this.averages[j] = (this.averages[j] || 0) + Number(n[j]);
-            });
-        });
-        _.forEach(needed, (j) => {
-            this.averages[j] = this.averages[j] / this.filtered.length;
-        });
-        this.averages.length = moment.duration(this.averages.length, 'seconds').format();
     }
 }
 
