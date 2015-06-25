84beb216c91b93f96369f51bc4575bc8b0a7f48e
cooper <scttcper@gmail.com>
2015-06-25 01:02:05 -0700

    enable babel runtime

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -12,8 +12,8 @@ function webpack(watch, callback) {
   var webpackOptions = {
     watch: watch,
     module: {
-      preLoaders: [{ test: /\.js$/, exclude: /node_modules/, loader: 'jshint-loader'}],
-      loaders: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}]
+      preLoaders: [{ test: /\.js$/, exclude: /(node_modules|bower_components)/, loader: 'jshint-loader'}],
+      loaders: [{ test: /\.js$/, exclude: /(node_modules|bower_components)/, loader: 'babel-loader?optional[]=runtime'}]
     },
     output: { filename: 'index.module.js' }
   };
