19d911893444c5eb3448961abe841cc428bffca0
cooper <scttcper@gmail.com>
2015-06-25 19:01:48 -0700

    keep trying with adsense

--- src/app/components/adsense/adsense.directive.js
+++ src/app/components/adsense/adsense.directive.js
@@ -5,21 +5,23 @@ class adsense {
         let directive = {
             restrict: 'E',
             scope: {},
-            template: '',
-            link: linkFunc
+            template: `<div style="padding-bottom:8px;"><ins class='adsbygoogle' style='display:block;min-height:60px;min-width:200px;margin-top:2px;' data-ad-client='ca-pub-7640562161899788' data-ad-slot='7259870550' data-ad-format='auto' data-ad-region='page-{{ ::ctrl.random }}'></ins></div>`,
+            controller: AdsenseController,
+            controllerAs: 'ctrl',
+            bindToController: true
         };
 
         return directive;
+    }
+}
 
-        function linkFunc(scope, el) {
-            let random = _.random(0, 1000000);
-            let html = `<ins class='adsbygoogle' style='display:block;min-height:60px;margin-top:2px;' data-ad-client='ca-pub-7640562161899788' data-ad-slot='7259870550' data-ad-format='auto' data-ad-region='page-${random}'></ins><br>`;
-            el.append(html);
-            setTimeout(function() {
-                let adsbygoogle = window.adsbygoogle || [];
-                adsbygoogle.push({});
-            }, 10);
-        }
+class AdsenseController {
+    constructor() {
+        this.random = Math.floor(Math.random() * 100000) + 1;
+        setTimeout(function() {
+            let adsbygoogle = window.adsbygoogle || [];
+            adsbygoogle.push({});
+        }, 10);
     }
 }
 
